6383bc4801581a7998ca6ec1a45e7e34e337569f
Sunpreet <sunpreet7275@gmail.com>
2025-12-10 15:11:33 +0530

    new port

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ app.get("*", (req, res) => {
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5002
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
